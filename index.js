b64f9efcdd0da7759577d5da1d3bd7ac08160fa6
Kled <1017yu@daum.net>
2021-12-27 23:52:00 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import _ from 'lodash';
+import _ from 'lodash'; // '_' 라는 변수에 할당해서 활용
 
 console.log('Hello World!');
-console.log(_.camelCase('hello world'));
+console.log(_.camelCase('hello world')); // lodash module의 camelCase method 실행
