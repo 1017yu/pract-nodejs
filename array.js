853738fa5d69d693e238d2e041e124c12f3a7073
Kled <1017yu@daum.net>
2022-01-24 17:07:06 +0900

    Update array.js

--- array.js
+++ array.js
@@ -12,3 +12,31 @@ console.log(numbers.concat(fruits))
 fruits.forEach(function (element, index, array) {
   console.log(element, index, array)
 })
+
+
+// map: 인수로 사용하는 콜백의 내부에서 반환하는 하나의 object 데이터를 배열로 반환
+
+const a = fruits.map((fruits, index) => {
+  return `${fruits}-${index}`
+})
+
+const b = fruits.map((fruits, index) => ({
+  id: index,
+  name: fruits
+}))
+
+console.log(a)
+console.log(b)
+
+const c = numbers.filter(number => number < 3)
+
+console.log(c)
+
+// .push() .unshift()
+// 원본 수정됨 주의
+
+numbers.push(5)
+console.log(numbers)
+
+numbers.unshift(0)
+console.log(numbers)
